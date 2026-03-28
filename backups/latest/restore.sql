87f92c13ba872f5570d98959e779ddbba8d2bf51
ghouthi7-crypto <ghouthi7@gmail.com>
2026-03-28 04:10:01 +0100

    Initial database backup 2026-03-28

---
+++ backups/latest/restore.sql
@@ -0,0 +1,70 @@
+# ================================================
+# UNIVEXAM - Complete Database Backup
+# Generated: 2026-03-28
+# Supabase Project: trjfjxvdeuwkzufbpqgh (univ-connect)
+# ================================================
+
+BEGIN;
+
+-- ============================================
+-- Table: universities (22 rows)
+-- ============================================
+DELETE FROM universities;
+
+INSERT INTO "id", "name_ar", "name_en", "wilaya", "created_at" VALUES ('847ccdc0-da35-4716-aab7-d1154579d11a', 'جامعة الجزائر 1', NULL, 'الجزائر', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('cd088f4a-7109-4cce-8164-0ef7ae169418', 'جامعة هواري بومدين للعلوم والتكنولوجيا', NULL, 'الجزائر', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('36c1fd5e-67a1-48b1-b0a3-75b74339a270', 'جامعة قسنطينة 1', NULL, 'قسنطينة', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('040b10db-ab5d-4d8b-a160-d7d08e4582fd', 'جامعة وهران 1 أحمد بن بلة', NULL, 'وهران', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('0eb722a2-0f83-404d-bca2-b94e57df0603', 'جامعة عنابة', NULL, 'عنابة', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('52ce91fa-c24b-4bdd-85fe-f0a46833ae91', 'جامعة سطيف 1 فرحات عباس', NULL, 'سطيف', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('ce0659e5-2bd0-475e-8126-6038399844d5', 'جامعة بجاية', NULL, 'بجاية', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('929dfc45-01cb-471a-934f-3a8aba0d610a', 'جامعة تلمسان', NULL, 'تلمسان', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('d7b07d27-c3ec-481e-b633-c04fbd1459fb', 'جامعة بسكرة محمد خيضر', NULL, 'بسكرة', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('f9f4512e-afc4-461b-8001-919e26708679', 'جامعة باتنة', NULL, 'باتنة', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('d2a1c1bc-d01b-42f2-ba44-738e0e2205db', 'جامعة تيزي وزو', NULL, 'تيزي وزو', '2026-03-18 06:01:44.750108+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('d59c40f1-8526-4490-b71a-1c9ab31523dd', 'DJELFA', NULL, '', '2026-03-21 21:41:37.933436+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('dac9c6e1-71b7-4731-acc2-ecf05bdd11ef', 'جامعة الشلف حسيبة بن بوعلي', NULL, 'الشلف', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('d16580b4-a516-4a7b-983b-e6f1cae8e19b', 'جامعة تيارت ابن خلدون', NULL, 'تيارت', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('cfabbbc5-5d8b-4679-8fcc-41bb8f7a2e22', 'جامعة مستغانم عبد الحميد بن باديس', NULL, 'مستغانم', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('40201f8a-9a61-4aed-970f-a999f9c07796', 'جامعة سيدي بلعباس جيلالي ليابس', NULL, 'سيدي بلعباس', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('04ec6881-ca8d-4c91-9ff1-40a4f45fdb22', 'جامعة معسكر مصطفى اسطمبولي', NULL, 'معسكر', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('a599da04-f25f-4785-9523-a75aa8aecf2b', 'جامعة الأغواط عمار ثليجي', NULL, 'الأغواط', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('73d3ea6e-8f98-473b-ab0d-4f2d788becc1', 'جامعة ورقلة قاصدي مرباح', NULL, 'ورقلة', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('f4d8f5cb-bb54-44b5-b836-2f2ce5f69bd5', 'جامعة بشار طاهري محمد', NULL, 'بشار', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('5a45b517-0899-4f3b-8648-f8e0b710281b', 'جامعة سكيكدة 20 أوت 1955', NULL, 'سكيكدة', '2026-03-27 17:05:47.10059+00');
+INSERT INTO universities ("id", "name_ar", "name_en", "wilaya", "created_at") VALUES ('f75702a0-1476-4aa6-b2a0-2a44dd7a423b', 'جامعة برج بوعريريج محمد البشير الإبراهيمي', NULL, 'برج بوعريريج', '2026-03-27 17:05:47.10059+00');
+
+-- ============================================
+-- Table: site_content (24 rows)
+-- ============================================
+DELETE FROM site_content;
+
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('admin_name', 'Mohamed BenGhouthi', '2026-03-25 03:01:11.370242+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('admin_avatar', 'https://pub-e354e3db406b475f95f0c08f1a94001e.r2.dev/admin-avatar-1774493537768.jpg', '2026-03-25 03:01:11.352849+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('admin_role', 'المدير العام', '2026-03-25 03:01:11.353016+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('admin_badge', 'diamond', '2026-03-25 04:07:14.370344+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('contact_email', 'univexame@gmail.com', '2026-03-23 19:02:04.178736+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('site_location', 'DJELFA', '2026-03-24 15:08:13.098752+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('important_links', '[]', '2026-03-24 15:08:13.111994+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('privacy_text', 'EFFECTIVEMENT ', '2026-03-22 05:05:16.342191+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('about_text', '', '2026-03-22 05:05:16.342191+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('contact_phone', '', '2026-03-26 04:23:38.407429+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('social_facebook', '', '2026-03-26 04:23:38.625212+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('social_instagram', '', '2026-03-26 04:23:38.813735+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('social_telegram', '', '2026-03-26 04:23:39.007842+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('playstore_link', '', '2026-03-26 04:23:39.225936+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_home_top', '', '2026-03-27 15:14:18.400603+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_home_bottom', '', '2026-03-27 15:14:18.677844+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_side_top', '', '2026-03-27 15:14:18.939305+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_side_bottom', '', '2026-03-27 15:14:19.167279+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_pdf_top', '', '2026-03-27 15:14:19.424966+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_pdf_bottom', '', '2026-03-27 15:14:19.652423+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_files_top', '', '2026-03-27 15:14:19.857659+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_files_bottom', '', '2026-03-27 15:14:20.216858+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('ad_popup', '', '2026-03-27 15:14:20.420829+00');
+INSERT INTO site_content ("key", "value", "created_at") VALUES ('admin_profile', '{"name":"Admin","avatar":"","role":"مدير النظام","badge":"diamond"}', '2026-03-27 14:05:38.473866+00');
+
+COMMIT;
+
+-- Backup generated: 2026-03-28 02:58 UTC
+-- Total rows: 22 universities + 24 site_content
