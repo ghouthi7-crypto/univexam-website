7c6e59b80dd4109108c84c47e0f217526276f7e9
ghouthi7-crypto <ghouthi7@gmail.com>
2026-04-06 01:09:40 +0100

    deploy: update assets/arrow-left-Cl1Uap6y-lM32RDkq-CprfKeh6-CEr4R__n.js

---
+++ assets/arrow-left-Cl1Uap6y-lM32RDkq-CprfKeh6-CEr4R__n.js
@@ -0,0 +1,6 @@
+import{O as e}from"./index-BvL8wq2T.js";/**
+ * @license lucide-react v0.577.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],o=e("arrow-left",r);export{o as e};
